869dfcdc75d03f8f3999f34df0eb17e58d0335c1
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-09 06:34:21 -0300

    Arquivo de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
+import {LogoutComponent} from './logout/logout.component';
+import {canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
+import {AuthorsComponent} from './authors/authors.component';
+import {GenresComponent} from './genres/genres.component';
+import {LocationsComponent} from './locations/locations.component';
+import {PublishersComponent} from './publishers/publishers.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'me', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, ...canActivate(() => redirectLoggedInTo(['me'])) },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'me', component: UserComponent, ...canActivate(() => redirectUnauthorizedTo(['login']))},
+
+  { path: 'authors', component: AuthorsComponent, ...canActivate(() => redirectUnauthorizedTo(['login']))},
+  { path: 'genres', component: GenresComponent, ...canActivate(() => redirectUnauthorizedTo(['login']))},
+  { path: 'locations', component: LocationsComponent, ...canActivate(() => redirectUnauthorizedTo(['login']))},
+  { path: 'publishers', component: PublishersComponent, ...canActivate(() => redirectUnauthorizedTo(['login']))},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
