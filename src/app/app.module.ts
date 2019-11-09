bb53b79619a6922daa075518f6d153d371f52b4b
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-09 06:34:09 -0300

    Módulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,36 @@ import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {environment} from '../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { LogoutComponent } from './logout/logout.component';
+import {AngularFireAuthGuardModule} from '@angular/fire/auth-guard';
+import { AuthorsComponent } from './authors/authors.component';
+import { GenresComponent } from './genres/genres.component';
+import { LocationsComponent } from './locations/locations.component';
+import { PublishersComponent } from './publishers/publishers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    LogoutComponent,
+    AuthorsComponent,
+    GenresComponent,
+    LocationsComponent,
+    PublishersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase, 'biblioteca')
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireAuthGuardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
