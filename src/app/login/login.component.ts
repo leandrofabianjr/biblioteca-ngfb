6f7baa75cc0bcfed8e9ceabc65018dea1b71334c
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-09 06:31:58 -0300

    Componente de login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../services/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+  loginError = false;
+
+  constructor(private auth: AuthService, private route: Router) { }
+
+  ngOnInit() {
+    this.auth.user.subscribe(u => {
+      if (u) {
+      console.log('Já logado');
+      } else {
+        console.log('Não logado');
+      }
+    });
+  }
+
+  googleLogin() {
+    this.auth
+      .googleLogin()
+      .subscribe(null, error => {
+        this.loginError = true;
+        console.error(error);
+      });
+  }
+}
