8c5b6d3b463e30f39f28dcdf288486d2010f0e73
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-09 06:32:19 -0300

    Componente de usuário

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService, AppUser} from '../services/auth.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.sass']
+})
+export class UserComponent implements OnInit {
+
+  constructor(public auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+}
