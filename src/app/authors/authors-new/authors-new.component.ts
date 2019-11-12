7b57ea21e1393c30dd1dc36099e9dc7182432298
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-12 06:43:49 -0300

    Criado componente de novo autor

---
+++ src/app/authors/authors-new/authors-new.component.ts
@@ -0,0 +1,43 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {AuthorsService} from '../../services/authors.service';
+import {Author} from '../../models/author';
+
+@Component({
+  selector: 'app-authors-new',
+  templateUrl: './authors-new.component.html',
+  styleUrls: ['./authors-new.component.sass']
+})
+export class AuthorsNewComponent implements OnInit {
+
+  authorForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private autSrv: AuthorsService,
+    public dialogRef: MatDialogRef<AuthorsNewComponent>,
+    @Inject(MAT_DIALOG_DATA) public author: Author) {}
+
+  ngOnInit() {
+    this.authorForm = this.fb.group({
+      nome: [this.author ? this.author.name : '', Validators.required]
+    });
+  }
+
+  salvar() {
+    if (this.authorForm.valid) {
+      let author = new Author();
+      author.id = this.author ? this.author.id : null;
+      author.name = this.authorForm.get('nome').value;
+
+      this.autSrv.save(author)
+        .subscribe(
+          aut => author = aut,
+          err => console.error('Erro ao salvar author', err),
+          () => this.dialogRef.close(author)
+        );
+    }
+  }
+
+}
