28ef6117860912b2a359dc43b12d3c3145230e71
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-17 12:01:58 -0300

    Atualizado código do componente de listagem de editoras

--- src/app/publishers/publishers.component.ts
+++ src/app/publishers/publishers.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-import {PublishersService} from '../services/publishers.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatDialog, MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {Publisher} from '../models/publisher';
+import {PublishersService} from '../services/publishers.service';
+import {ItemsService} from '../services/items.service';
+import {first} from 'rxjs/operators';
+import {DialogInfoComponent} from '../dialog-info/dialog-info.component';
+import {DialogConfirmationComponent} from '../dialog-confirmation/dialog-confirmation.component';
+import {PublishersNewComponent} from '../publishers/publishers-new/publishers-new.component';
 
 @Component({
   selector: 'app-publishers',
@@ -9,12 +14,53 @@ import {Publisher} from '../models/publisher';
   styleUrls: ['./publishers.component.scss']
 })
 export class PublishersComponent implements OnInit {
-  publishers: Observable<Publisher[]>;
+  loading = true;
+  displayedColumns: string[] = ['name'];
+  publishersSource = new MatTableDataSource<Publisher>();
+  private publishers: Publisher[];
 
-  constructor(private pubSrv: PublishersService) {
-    this.publishers = pubSrv.data;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor(private pubSrv: PublishersService, public dialog: MatDialog, private itmSrv: ItemsService) { }
+
+  ngOnInit() {
+    this.pubSrv.data.subscribe(pubs => {
+      this.publishers = pubs;
+      this.publishersSource.data = pubs;
+      this.loading = false;
+    });
+    this.publishersSource.paginator = this.paginator;
+    this.publishersSource.sort = this.sort;
   }
 
-  ngOnInit() { }
+  search(column: string, term: string) {
+    this.publishersSource.data = !term
+      ? this.publishers
+      : this.publishers.filter(l => l.name.toLowerCase().includes(term.toLowerCase()));
+  }
 
+  remove(publisher: Publisher) {
+    this.itmSrv.data.pipe(first()).subscribe(itms => {
+      if (itms.find(i => i.publishers.find(a => a.id === publisher.id) !== undefined)) {
+        this.dialog.open(DialogInfoComponent,
+          { data: {title: 'Desculpe...', message: 'Não é possível remover esta editora. Há itens relacionados a ela.'}});
+      } else {
+        this.dialog.open(DialogConfirmationComponent).afterClosed()
+          .subscribe((res: boolean) => {
+            if (res) {
+              this.pubSrv.delete(publisher.id)
+                .subscribe(
+                  itm => null,
+                  err => console.error('Erro ao remover item')
+                );
+            }
+          });
+      }
+    });
+  }
+
+  edit(publisher: Publisher) {
+    this.dialog.open(PublishersNewComponent, { data: publisher });
+  }
 }
