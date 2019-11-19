b035669ec25f7f4d82439f61e1ba52dab86130a4
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-19 20:04:08 -0300

    Removendo uso de helper canActivate do angularfire das rotas pois não funcionou em produção

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {UserComponent} from './user/user.component';
 import {LogoutComponent} from './logout/logout.component';
-import {canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
+import {AngularFireAuthGuard, canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
 import {AuthorsComponent} from './authors/authors.component';
 import {GenresComponent} from './genres/genres.component';
 import {LocationsComponent} from './locations/locations.component';
@@ -12,23 +12,24 @@ import {ItemsComponent} from './items/items.component';
 import {ItemsNewComponent} from './items/items-new/items-new.component';
 import {LoggedComponent} from './logged/logged.component';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/login']);
 
 const routes: Routes = [
   { path: '', redirectTo: 'u/me', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, ...canActivate(() => redirectLoggedInTo(['me'])) },
 
-  { path: 'u', component: LoggedComponent, ...canActivate(() => redirectUnauthorizedTo(['/login'])), children: [
-    { path: 'logout', component: LogoutComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
-    { path: 'me', component: UserComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
+  { path: 'u', component: LoggedComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }, children: [
+    { path: 'logout', component: LogoutComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+    { path: 'me', component: UserComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
 
-    { path: 'authors', component: AuthorsComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
-    { path: 'genres', component: GenresComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
-    { path: 'locations', component: LocationsComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
-    { path: 'publishers', component: PublishersComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
-    { path: 'items', ...canActivate(() => redirectUnauthorizedTo(['/login'])), children: [
-      { path: '', component: ItemsComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
-      { path: 'new', component: ItemsNewComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
-      { path: ':id', component: ItemsNewComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
+    { path: 'authors', component: AuthorsComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+    { path: 'genres', component: GenresComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+    { path: 'locations', component: LocationsComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+    { path: 'publishers', component: PublishersComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+    { path: 'items', canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }, children: [
+      { path: '', component: ItemsComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+      { path: 'new', component: ItemsNewComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+      { path: ':id', component: ItemsNewComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
     ]}
   ]}
 ];
