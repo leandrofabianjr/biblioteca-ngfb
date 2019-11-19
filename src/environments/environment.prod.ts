91451471715b716a841666c988df84d455a52819
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-18 21:30:47 -0300

    Adicionado dados do firebase ao ambiente de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC3YkqUXdQ0JEKGLLVYViQQdby1Ct9fQBM',
+    authDomain: 'biblioteca-lfj.firebaseapp.com',
+    databaseURL: 'https://biblioteca-lfj.firebaseio.com',
+    projectId: 'biblioteca-lfj',
+    storageBucket: 'biblioteca-lfj.appspot.com',
+    messagingSenderId: '504422520921',
+    appId: '1:504422520921:web:baa93b120865ea253c3948',
+    measurementId: 'G-TCGY9T6XSM'
+  }
 };
