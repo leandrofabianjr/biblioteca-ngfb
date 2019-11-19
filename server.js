f08202d1df56f324f4b483b388fab8ea469afe8b
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-19 20:29:05 -0300

    Instalado biblioteca express que estava faltando

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/biblioteca-ngfb'));
+app.use(express.static(__dirname + '/dist/biblioteca-ngfb'));
+
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname,'/dist/biblioteca-ngfb/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
